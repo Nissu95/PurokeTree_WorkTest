1de257ec5aa6533f1a502c964567e75c5421b073
Nicolas Susterman <nicosus95@hotmail.com>
2021-04-12 19:03:31 -0300

    +Rock gravity, Add forces

---
+++ WorkTestUnity/Assets/Scripts/PhysicsScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsScript : MonoBehaviour
+{
+    [SerializeField] float gravity;
+
+    Vector2 velocity = Vector2.zero;
+    //RaycastHit2D hit2D;
+
+    void FixedUpdate()
+    {
+        AddForceY(-gravity);
+
+        //Physics2D.Raycast(transform, )
+
+        transform.Translate(velocity);
+    }
+
+    public void AddForceX(float x)
+    {
+        velocity.x = x * Time.fixedDeltaTime;
+    }
+
+    public void AddForceY(float y)
+    {
+        velocity.y = y * Time.fixedDeltaTime;
+    }
+
+}
