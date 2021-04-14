1fe3cef184dc5bec05fdd25f0de97d2926a9690e
Nicolas Susterman <nicosus95@hotmail.com>
2021-04-14 18:36:51 -0300

    +Camera Follow Player to Certain Point

--- WorkTestUnity/Assets/Scripts/CameraMovement.cs
+++ WorkTestUnity/Assets/Scripts/CameraMovement.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] Transform minCameraPos;
-    [SerializeField] Transform maxCameraPos;
     [SerializeField] Transform target;
+    [SerializeField] float minCameraPos;
+    [SerializeField] float maxCameraPos;
+    [SerializeField] float followSpeed;
     [SerializeField] Vector3 offset;
+
+    Vector3 velocity = Vector3.zero;
+
+    Vector3 targetPos;
+    Vector3 smoothPos;
 
     void FixedUpdate()
     {
-        transform.position = target.position + offset;
+        targetPos = target.position + offset;
+        targetPos.x = Mathf.Clamp(targetPos.x, minCameraPos, maxCameraPos);
+        smoothPos = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, followSpeed * Time.fixedDeltaTime);
+
+        transform.position = smoothPos;
     }
 }
