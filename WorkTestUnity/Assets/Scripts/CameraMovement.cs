8885c118612c8b9c423263ab2da43c49a085839f
Nicolas Susterman <nicosus95@hotmail.com>
2021-04-14 17:25:38 -0300

    +Camera Follow Player

---
+++ WorkTestUnity/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform minCameraPos;
+    [SerializeField] Transform maxCameraPos;
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset;
+
+    void FixedUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
