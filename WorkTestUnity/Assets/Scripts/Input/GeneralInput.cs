2dced7ea57c2e336cf374c7448ebc9b704b89f1c
Nicolas Susterman <nicosus95@hotmail.com>
2021-04-20 12:05:02 -0300

    +Input Manager, PCInput

---
+++ WorkTestUnity/Assets/Scripts/Input/GeneralInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralInput
+{
+    protected enum InputState
+    {
+        Idle,
+        Down,
+        Held,
+        Up
+    }
+
+    protected InputState inputState;
+    protected float moveInput;
+
+    public virtual void Update() { }
+    public virtual bool GetInputDown() { return false; }
+    public virtual bool GetInputUp() { return false; }
+    public virtual float GetMoveInput() { return 0; }
+}
---
+++ WorkTestUnity/Assets/Scripts/Input/PCInput.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCInput : GeneralInput
+{
+    float deadZone = 0.02f;
+
+    public override void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        switch (inputState)
+        {
+            case InputState.Idle:
+                if (Input.GetAxis("Horizontal") < -deadZone || Input.GetAxis("Horizontal") > deadZone)
+                {
+                    inputState = InputState.Down;
+                }
+                break;
+
+            case InputState.Down:
+                inputState = InputState.Held;
+                break;
+
+            case InputState.Held:
+                if (Input.GetAxis("Horizontal") > -deadZone && Input.GetAxis("Horizontal") < deadZone)
+                {
+                    inputState = InputState.Up;
+                }
+                break;
+
+            case InputState.Up:
+                inputState = InputState.Idle;
+                break;
+        }
+    }
+
+    public override bool GetInputDown()
+    {
+        if (inputState == InputState.Down)
+            return true;
+        else
+            return false;
+    }
+
+    public override bool GetInputUp()
+    {
+        if (inputState == InputState.Up)
+            return true;
+        else
+            return false;
+    }
+
+    public override float GetMoveInput()
+    {
+        return moveInput;
+    }
+
+}
